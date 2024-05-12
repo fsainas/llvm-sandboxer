22d065e95e49ecd5c6e8845cf63b724aab349dc5
Franco Sainas <franco@sainas.me>
2024-05-12 19:56:04 +0200

    feat: add benchmark

---
+++ benches/bench.rs
@@ -0,0 +1,110 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::process::Command;
+use std::path::Path;
+use inkwell::module::Module;
+use inkwell::context::Context;
+use llvm_sandboxer::runtime;
+
+/// Compile test cases's C sources.
+fn compile_c_files() {
+    let no_utx_dir_path = Path::new("benches/c_files/no_utx/");
+    let utx_dir_path = Path::new("benches/c_files/utx/");
+
+    // Compile the C source file
+    Command::new("make")
+        .args(["-C", &no_utx_dir_path.to_string_lossy()])
+        .output()
+        .expect("Failed to compile C source file.");
+
+    // Compile the C source file
+    Command::new("make")
+        .args(["-C", &utx_dir_path.to_string_lossy()])
+        .output()
+        .expect("Failed to compile C source file.");
+}
+
+/// Compile instrumented LLVMs to executable
+fn compile_ll_to_exec(filepath: &str) -> String {
+    let filepath = Path::new(filepath);
+    let exec_filepath = filepath.with_extension("o");
+
+    // Compile
+    let output = Command::new("clang")
+        .arg(filepath)
+        .arg("-o")
+        .arg(exec_filepath.clone())
+        .output()
+        .expect("Failed to compile LLVMs to executable.");
+
+    println!("{:?}", output);
+
+    exec_filepath.display().to_string()
+}
+
+/// Test one LLVM bitcode file.
+fn instrument_test_case(test_case_name: &str) -> String {
+
+    // Get testcase bitcode path
+    let bitcode_path = format!("target/bench/utx/{}.bc", test_case_name);
+    let bitcode_path = Path::new(&bitcode_path);
+    
+    // Compile if it not exists
+    if !bitcode_path.exists() {
+        compile_c_files();
+    }
+
+    // Parse bitcode
+    let context = Context::create();
+    let module = Module::parse_bitcode_from_path(&bitcode_path, &context).unwrap();
+
+    match runtime::instrument(test_case_name, &context, &module, true) {
+        Ok(()) => println!("Instrumentation completed successfully"),
+        Err(err) => println!("Error occurred: {:?}", err)
+    }
+
+    // Save to file
+    let filepath = format!("target/bench/instrumented/{}_instrumented.ll", test_case_name);
+    let _ = module.print_to_file(filepath.clone());
+
+    return filepath
+}
+
+fn benchmark_0(c: &mut Criterion) {
+
+    let test_case_name = "benchmark_0";
+
+    let exec_path = format!("target/bench/no_utx/{}.o", test_case_name);
+    let exec_path = Path::new(&exec_path);
+
+    // Compile if it not exists
+    if !exec_path.exists() {
+        compile_c_files();
+    }
+
+    c.bench_function("benchmark_0", |b| {
+        b.iter(|| {
+            let _ = Command::new(exec_path)
+                .output()
+                .expect(&format!("Cannot execute {}.", exec_path.display()));
+        });
+    });
+}
+
+fn benchmark_0_instrumented(c: &mut Criterion) {
+    let test_case_name = "benchmark_0";
+
+    let ll_filepath = instrument_test_case(test_case_name);
+    let exec_path = compile_ll_to_exec(&ll_filepath);
+    let exec_path = Path::new(&exec_path);
+
+    c.bench_function("instrumented_benchmark_0", |b| {
+        b.iter(|| {
+            let _ = Command::new(exec_path)
+                .output()
+                .expect(&format!("Cannot execute {}.", exec_path.display()));
+        });
+    });
+}
+
+criterion_group!(benches, benchmark_0, benchmark_0_instrumented);
+criterion_main!(benches);
---
+++ benches/c_files/no_utx/benchmark_0.c
@@ -0,0 +1,43 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ARR_LENGTH 10000000
+
+void utx0(void);
+void utx1(void *addr, size_t len);
+
+
+static uint64_t shared_array[ARR_LENGTH];
+
+
+uint64_t benchmark_0()
+{
+
+    srand(time(NULL));
+
+    // Init array
+    for (int i = 0; i < ARR_LENGTH; i++) {
+        shared_array[i] = rand() % 100;
+    }
+
+    // Compute the sum
+    uint64_t sum = 0;
+    for (int i = 0; i < ARR_LENGTH; i++) {
+        sum += shared_array[i];
+    }
+
+    return sum;
+	
+}
+
+int main()
+{
+    uint64_t sum = 0;
+    for (int i = 0; i < 100; i++) {
+        sum += benchmark_0();
+    }
+    printf("Sum: %lu\n", sum);
+	return 0;
+}
